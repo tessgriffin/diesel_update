869b711b240e9a934ad99c56d422a4b657e4dc97
Tess Griffin <tessjgriffin@gmail.com>
2016-02-04 11:58:00 -0700

    Inital commit for diesel update through show posts

---
+++ src/bin/show_posts.rs
@@ -0,0 +1,24 @@
+extern crate diesel_update;
+extern crate diesel;
+
+use self::diesel_update::*;
+use self::diesel_update::models::*;
+use self::diesel::prelude::*;
+
+fn main() {
+    use diesel_update::schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts.filter(published.eq(true))
+        .limit(5)
+        .load(&connection)
+        .expect("Error loading posts")
+        .collect::<Vec<Post>>();
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
+    }
+}
